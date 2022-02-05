deb7fd2420be6e7f88ced1f6a5627bfe86590584
geo <franklinjazz@gmail.com>
2022-02-05 09:00:15 -0500

    Maven project start

---
+++ src/main/java/com/inditex/zaraApi/ZaraApiApplication.java
@@ -0,0 +1,13 @@
+package com.inditex.zaraApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaraApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inditex/zaraApi/ZaraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inditex.zaraApi;
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
